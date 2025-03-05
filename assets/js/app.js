1a2a39ca4ae08ef76cdaed51d74a8699d7568911
jatin-rajput563 <jkt3298@gmail.com>
2025-03-05 12:09:29 +0530

    section-4-complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,36 @@ const navbarbtn = () => {
     document.getElementById("rotates-2").classList.toggle("rotate-45")
     document.body.classList.toggle("overflow-hidden")
 }
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
